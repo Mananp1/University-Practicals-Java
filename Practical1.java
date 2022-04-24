d272b45798836bda73298ef976f9752efb88b164
Mananp1 <91424574+Mananp1@users.noreply.github.com>
2022-04-24 03:14:09 -0700

    Create Practical1.java

---
+++ Practical1.java
@@ -0,0 +1,8 @@
+/*Write a Program that displays Welcome to Java, This is my first JAVA program.*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java, This is my first JAVA program.");
+    }
+}
+
