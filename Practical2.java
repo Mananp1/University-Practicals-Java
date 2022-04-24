e7c2c60130f4a69da539bd0e3e95e8d2c03f6b8d
Mananp1 <91424574+Mananp1@users.noreply.github.com>
2022-04-24 03:24:38 -0700

    Create Practical2.java

---
+++ Practical2.java
@@ -0,0 +1,13 @@
+/*Write a program to convert rupees to dollar. 70 rupees=1 dollar. */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner convert = new Scanner(System.in);
+        System.out.println("Enter the amount: ");
+        int rupees = convert.nextInt();
+        float dollers = (float)(rupees/70.0);
+        System.out.printf("%d = %f$",rupees,dollers);
+    }
+}
