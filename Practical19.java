e82b567002a945a4fb0b22d240ca7f9f5f8d8247
Mananp1 <91424574+Mananp1@users.noreply.github.com>
2022-04-24 09:25:32 -0700

    Create Practical19.java

---
+++ Practical19.java
@@ -0,0 +1,17 @@
+/*Write a program to show divide by zero error through exception, and also try to catch 
+the exception.*/
+
+import java.io.*;
+class Exception{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Divided by zero operation cannot possible.");
+        }
+    }
+}
