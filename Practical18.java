9990d4efd448a0a5941b44cc681562778b7944e3
Mananp1 <91424574+Mananp1@users.noreply.github.com>
2022-04-24 09:22:27 -0700

    Create Practical18.java

---
+++ Practical18.java
@@ -0,0 +1,16 @@
+/*Write a java program to demonstrate the concept of interface.*/
+
+interface Language {
+    void getName(String name);
+}
+class ProgrammingLanguage implements Language {
+    public void getName(String name) {
+        System.out.println("Programming Language: " + name);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        ProgrammingLanguage language = new ProgrammingLanguage();
+        language.getName("Java");
+    }
+}
