c1605f8f8e74b68075b79f0fc7e582cb17d758cc
Mananp1 <91424574+Mananp1@users.noreply.github.com>
2022-04-24 07:06:30 -0700

    Update Practical14.java

--- Practical14.java
+++ Practical14.java
@@ -1,17 +1,7 @@
 /*
-Write a java program to find out the area of circle and area of rectangle using the 
+Write a java program to find out the area of circle and area of rectangle using the
 concept of method overloading.
 */
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        CalculateArea ob = new CalculateArea();
-        ob.area(10,12);
-        ob.area(5.5);
-    }
-}
 class CalculateArea
 {
     void area(float x, float y)
@@ -22,5 +12,14 @@ class CalculateArea
     {
         double z = 3.14 * x * x;
         System.out.println("The area of the circle is "+z+" sq units.");
+    }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        CalculateArea ob = new CalculateArea();
+        ob.area(10,12);
+        ob.area(5.5);
     }
 }
